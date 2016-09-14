04a83a9e817c50fc3a96d1b12074b946725a92c0
Denis <denistrator@yandex.ua>
2016-09-14 16:44:07 +0300

    project init. add header, slider, cards

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(function(){
+	// $('.h-cards__img-holder').height($('.h-cards__elem').height() + 1);
+	// $('.h-cards__hover').height($('.h-cards__elem').height() + 1);
+
+	$('.js-fine-align').each(function() {
+		if ($(this).height() > $(this).width()){
+			$(this).addClass('js-fine-align-portrait');
+		} else {
+			$(this).addClass('js-fine-align-landscape');
+		}
+	});
+
+    var mainSwiper = new Swiper ('.main-slider', {
+      direction: 'horizontal',
+      loop: true,
+      speed: 400,
+	  // autoplay: 3500
+    })        
+
+})
